36f63424d160a60b0d312fea7b02535014820d2f
Jeferson Oliveira <zuruckyt@gmail.com>
2023-05-13 19:25:12 -0300

    chore: add tailwind and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
+			<header className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-neutral-100 hover:bg-sky-800">
 				<p>Ramiel</p>
 			</header>
 		</div>
